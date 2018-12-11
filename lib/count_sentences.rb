f8a62e9c1d619661bc5b3867720651430baf55b1
Marcus Plenty <marcusplenty@gmail.com>
2018-12-11 21:32:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
+
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
   def count_sentences
+    arr1 = self.split(" ")
+    counter = 0
+    arr1.each do |ele|
+      if ele[-1] == "!" || ele[-1] == "." || ele[-1] == "?"
+        counter += 1
+      end
+    end
+    return counter
 
   end
 end
